7c08c05f902fd23b7b145e1bd1a715a135efa46d
GYoung <ytok1108@aol.com>
2024-11-24 19:01:22 +0900

    zig arrays

--- 03_arrays/a01_arrays/README.md
+++ 03_arrays/a01_arrays/README.md
@@ -1,5 +1,17 @@
 # Result
 
+```bash
+$ zig build run
+
+info: { 1, 2, 3, 4, 5 }
+info: { 0, 0, 0, 0, 0 }
+info: { 65, 100, 101, 98, 97, 121, 111 }
+info: 7
+[A][d][e][b][a][y][o]
+```
+
+- zig build run
+
 ```zig
 const std = @import("std");
 
--- 03_arrays/a01_arrays/src/main.zig
+++ 03_arrays/a01_arrays/src/main.zig
@@ -1,9 +1,22 @@
 const std = @import("std");
+const print = std.debug.print;
 
 pub fn main() anyerror!void {
     const array: [5]u8 = .{ 1, 2, 3, 4, 5 };
+    const undefined_array: [5]u8 = undefined;
+    const name = "Adebayo";
+    const name_len = "Adebayo";
+    const print_name = "Adebayo";
 
     std.log.info("{any}", .{array});
+    std.log.info("{any}", .{undefined_array});
+    std.log.info("{any}", .{name});
+    std.log.info("{any}", .{name_len.len});
+
+    for (print_name) |ch| {
+        print("[{c}]", .{ch});
+    }
+    print("\n", .{});
 }
 
 test "simple test" {}
